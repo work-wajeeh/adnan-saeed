76c36db05ec9430733d7b220b94d5e671d62d9c0
Wajeeh Ahsan <work.wajeehahsan@gmail.com>
2026-04-17 11:03:29 +0500

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/adnan-saeed/',
   plugins: [react(), tailwindcss()],
 })
